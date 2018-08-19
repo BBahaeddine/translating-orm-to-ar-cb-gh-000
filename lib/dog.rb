ea64ff5af78eb7b2d880a952403097de67a1e050
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-19 17:15:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,13 @@
-class Dog
+class Dog < ActiveRecord::Base 
+  attr_accessor :name, :breed 
+  
+  def initialize(args = {})
+    @name = args[:name]
+    @breed = args[:breed]
+  end
+  
+  def self.create(args)
+    dog = self.new(args)
+    dog.save
+  end
 end
