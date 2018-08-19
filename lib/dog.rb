90a628236e7866620fa4abf789110832bc7846ca
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-19 16:02:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+class Dog < ActiveRecord::Base
+  attr_accessor :name, :breed
+  
+  
+  def save
+    self.save
+  end
+  
+  def update(args)
+    # dog = self.class.find_by_name(self.name)
+    # self.name = dog.name
+    # self.breed = dog.breed
+    # self.save
+  end
+  
+  def self.find_or_create_by()
+    
+  end
+  
+  def find_by_name(nom:)
+    self.find_by(name: nom)
+  end
 end
